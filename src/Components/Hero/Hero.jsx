74e1977ea42b70296be53c4b450c46b2c05bfb20
akanie8 <docsean2001@gmail.com>
2024-12-19 13:00:08 +0200

    changed cv

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/me.jpg";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-import resume from "../../assets/HS_ZITHA-CV.pdf";
+
 const Hero = () => {
   return (
     <div id="home" className="hero">
-      <img src={profile_img} alt="" />
+      <img src={profile_img} alt="Shaun Zitha" />
       <h1>
-        {" "}
-        <span>I'm Shaun Zitha,</span> Mobile Developer based in South Africa{" "}
+        <span>I'm Shaun Zitha,</span> Mobile Developer based in South Africa
       </h1>
       <p>
-        {" "}
         Computer Science & Informatics Graduate from the University Of
         Johannesburg
       </p>
@@ -23,7 +21,7 @@ const Hero = () => {
           </AnchorLink>
         </div>
         <div className="hero-resume">
-          <a href={resume} download="HS_ZITHA-CV.pdf">
+          <a href="/HS_ZITHA-CV.pdf" download="HS_ZITHA-CV.pdf">
             Download Resume
           </a>
         </div>
